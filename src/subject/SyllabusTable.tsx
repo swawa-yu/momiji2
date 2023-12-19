f47b03885d6cb5dd15e59da603537a8fd0d7b59c
swawa-yu <swawa.yu@gmail.com>
2023-12-20 02:57:59 +0900

    react-tableを使って表示するように変更

--- src/subject/SyllabusTable.tsx
+++ src/subject/SyllabusTable.tsx
@@ -1,142 +1,91 @@
-// import React, { Component } from "react";
+import React from 'react';
+import { useTable } from 'react-table';
 
 import {
-    // Subject,
-    // initializeSubject,
     subjectCodeList,
     subjectMap,
-    propertyToShowList
-} from '.';
+    propertyToShowList,
+} from './';
 
-// import { VariableSizeGrid, GridChildComponentProps } from "react-window";
+import './SyllabusTable.css';
+import { SearchOptions } from './search';
 
-// import styles from './SyllabusTable.module.css'
-import './SyllabusTable.css'
-
-// import { PropsFromToggle } from "react-bootstrap/esm/DropdownToggle";
-// import * as timetable from './timetable';
-// import * as bookmark from './bookmark';
-// import codeTypes from './code-types.json';
-// import { matchesSearchOptions, SearchOptions } from './search';
-// import { renderSubjectAsTableRow, renderSubjectForMobile } from './render';
-
-// TODO 再帰関数の実装になっているが、なぜ？
+// TODO 要実装
 // reactなら元データsearch option 変えるだけでできるんじゃね
-// const updateTable = (options: SearchOptions, index: number, displayedIndex: number) => {
-//     export const updateTable = () => {
-//         subjectCodeList.forEach(subjectCode => {
-//             subjectMap[subjectCode]
-//         });
-//     }
-// }
+const updateTableWithSearchOptions = (
+    options: SearchOptions,
+    index: number,
+    displayedIndex: number
+) => {
+    subjectCodeList.forEach((subjectCode) => {
+        subjectMap[subjectCode];
+    });
+};
 
-// function SyllabusTableHead() {
-//     return (
-//         <tr key="table-head">
-//             {
-//                 subjectCodeList.map(subjectCode, index) => (
-//             <td>{index}</td>
-//             <td>{subjectMap[subjectCode].name}</td>
-//             <td>{subjectMap[subjectCode].code}</td>
-//             )
-//             }
-//         </tr>
-//     );
-// }
-// function SyllabusTableRow(props: PropSubjectCode) {
-//     return (
-//         <tr key={props.subjectCode}>
-//             <td>{props.subjectCode}</td>
-//             <td>{subjectMap[props.subjectCode].name}</td>
-//         </tr >
-//     )
-// }
-
-// const Cell = ({ columnIndex, rowIndex, style }: GridChildComponentProps) => (
-//     (rowIndex == 0) ? (
-//         <div className={styles.cell} style={style}>
-//             {/* Item {rowIndex},{columnIndex} */}
-//             {propertyToShowList[columnIndex]}
-//             {/* {columnNameList[columnIndex]} */}
-//         </div >
-//     ) : (
-//         <div className={styles.cell} style={style}>
-//             {/* Item {rowIndex},{columnIndex} */}
-//             {subjectMap[subjectCodeList[rowIndex - 1]][propertyToShowList[columnIndex]]}
-//             {/* {columnNameList[columnIndex]} */}
-//         </div >
-//     )
-// );
-
-
-// const columnWidths: { [key: string]: number } = {
-//     "講義コード": 80,
-//     "開講部局": 100,
-//     "授業科目名": 200,
-//     "単位": 40,
-// }
-
-// TODO (オ)　のオープン科目の扱い
-
-export let numberOfSubjectsToShow = 100
+// TODO (オ) のオープン科目の扱い
+export let numberOfSubjectsToShow = 100;
 
 function SyllabusTable() {
     // initializeSubject();
-    console.log(propertyToShowList)
+    console.log(propertyToShowList);
     subjectCodeList.forEach((subjectCode) => {
-        if (subjectCode == "10000100") {
-            console.log(subjectCode)
+        if (subjectCode === '10000100') {
+            console.log(subjectCode);
         }
-    }
-    )
-    // updateTable();
+    });
+    // updateTableWithSearchOptions();
+
+    const data = React.useMemo(
+        () => subjectCodeList.map(subjectCode => subjectMap[subjectCode]),
+        [subjectCodeList, subjectMap]
+    );
+
+    const columns = React.useMemo(
+        () => propertyToShowList.map(columnName => ({
+            Header: columnName,
+            accessor: columnName,
+        })),
+        [propertyToShowList]
+    );
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable({
+        columns,
+        data,
+    });
 
     return (
-        <table>
+        <table {...getTableProps()} className="your-table-class">
             <thead>
-                {
-                    // <tr key="table-head">
-                    <tr>
-                        {
-                            propertyToShowList.map((columnName) =>
-                                <td>{columnName}</td>
-                            )
-                        }
-                    </tr >
-                }
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map(column => (
+                            <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                        ))}
+                    </tr>
+                ))}
             </thead>
-            <tbody>
-                {
-                    subjectCodeList.slice(0, numberOfSubjectsToShow).map((subjectCode) =>
-                        <tr>
-                            {
-                                propertyToShowList.map((columnName) =>
-                                    <td>{subjectMap[subjectCode][columnName]}</td>
+            <tbody {...getTableBodyProps()}>
+                {rows.map(row => {
+                    prepareRow(row);
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map(cell => {
+                                return (
+                                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
                                 )
-                            }
-                        </tr >
-                    )}
+                            })}
+                        </tr>
+                    )
+                })}
             </tbody>
-        </table >
-    )
-
-
-    // react-table を使った実装
-    // return (
-    //     <VariableSizeGrid
-    //         className={styles.grid}
-    //         width={1000}
-    //         height={900}
-    //         columnCount={columnNameList.length}
-    //         // columnCount={100}
-    //         // rowCount={Object.keys(subjectMap).length}
-    //         rowCount={10000}
-    //         columnWidth={(index) => (columnNameList[index] in columnWidths ? columnWidths[columnNameList[index]] : 200)}
-    //         rowHeight={(index) => 70}
-    //     >
-    //         {Cell}
-    //     </VariableSizeGrid>
-    // )
+        </table>
+    );
 }
 
-export default SyllabusTable
+export default SyllabusTable;
