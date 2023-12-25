d8b7654269ed41788853e63979b27fe19f97800e
swawa-yu <swawa.yu@gmail.com>
2023-12-25 14:10:55 +0900

    表の見た目を改善

--- src/subject/LectureUnit.tsx
+++ src/subject/LectureUnit.tsx
@@ -23,22 +23,18 @@ const LectureUnit: React.FC<LectureUnitProps> = ({ subjectData }) => {
         <div className="lecture-details">
             <div className="star-button">★</div>
             <div className="lecture-code-name">
-                <div className="lecture-code">講義コード</div>
-                <div className="lecture-name">授業科目名</div>
-            </div>
-            <div className="instructor">担当教員名</div>
+                <div className="lecture-code">{subjectData["講義コード"]}</div>
+                <div className="lecture-name">{subjectData["授業科目名"]}</div> </div>
+            <div className="instructor">{subjectData["担当教員名"]}</div>
             <div className="lecture-details-columns">
-                <div>開設期</div>
-                <div>開講キャンパス</div>
-                <div>対象学生</div>
-                <div>使用言語</div>
+                <div>{subjectData["開設期"]}</div>
+                <div>{subjectData["開講キャンパス"]}</div>
+                <div>{subjectData["対象学生"]}</div>
+                <div>{subjectData["使用言語"]}</div>
             </div>
             <div className="lecture-details-info">
-                <div>授業の目標・概要等</div>
-                <div>予習・復習へのアドバイス</div>
-                <div>履修上の注意 受講条件等</div>
-                <div>メッセージ</div>
-                <div>その他</div>
+                <div>{subjectData["授業の目標・概要等"]}</div>
+                <div>{subjectData["メッセージ"]}</div>
             </div>
         </div>
     );
