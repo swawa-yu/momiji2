932e1e2d70bf22116d6189c7dd45563c91fc3322
swawa-yu <swawa.yu@gmail.com>
2023-12-29 18:26:33 +0900

    表の見た目を改善

--- src/table-view/SubjectUnitComponent.tsx
+++ src/table-view/SubjectUnitComponent.tsx
@@ -37,14 +37,14 @@ const SubjectUnitComponent: React.FC<SubjectUnitComponentProps> = ({ subject, is
                 <div className="lecture-name">{subject["授業科目名"]}</div>
             </div>
             {/* 広島大学研究者総覧：https://seeds.office.hiroshima-u.ac.jp/search/result.html?f&p&n=名字+名前&rf&rg&as&cc&mAdvance&lang=ja */}
-            <div className="instructor">
+            <div className="teacher">
                 <ul>
-                    {subject["担当教員名"].split(',').map((instructor, index) => {
-                        const query = encodeURIComponent(instructor.split(' ').join(' '));
+                    {subject["担当教員名"].split(',').map((teacher, index) => {
+                        const query = encodeURIComponent(teacher.split(' ').join(' '));
                         const researchMapUrl = `https://researchmap.jp/researchers?q=${query}`;
                         return (
                             <li key={index}>
-                                <a href={researchMapUrl} target="_blank" rel="noopener noreferrer">{instructor}</a>
+                                <a href={researchMapUrl} target="_blank" rel="noopener noreferrer">{teacher}</a>
                             </li>
                         );
                     })}
@@ -53,28 +53,18 @@ const SubjectUnitComponent: React.FC<SubjectUnitComponentProps> = ({ subject, is
 
 
 
-            <div className="lecture-details-columns">
-                <div>{subject["開設期"]}</div>
-                <div>{subject["曜日・時限・講義室"]}</div>
-                {/* <div>セメスター：{kaisetsuki.semester}</div> */}
-                {/* <div>履修年次：{kaisetsuki.rishuNenji}</div> */}
-                {/* <div>時期区分(開設期)：{kaisetsuki.jikiKubun}</div> */}
-                {/* <div>時期区分(schedules)：{schedules[0].jikiKubun}</div> */}
-                {/* <div>曜日：{schedules[0].jigen?.youbi[0]}</div> */}
-                {/* <div>時限Range：{schedules[0].jigen?.jigenRange[0]}-{schedules[0].jigen?.jigenRange[1]}</div> */}
-                {/* <div>コマRange：{schedules[0].jigen?.komaRange[0]}-{schedules[0].jigen?.komaRange[1]}</div> */}
-                {/* <div>
-                    {schedules[0].rooms.map((room, index) => (
-                        <div key={index}>講義室：{room}</div>
-                    ))}
-                </div> */}
-                <div>{subject["開講キャンパス"]}</div>
-                {/* <div>対象学生：{subject["対象学生"]}</div> */}
-                <div>{subject["使用言語"]}</div>
+            {/* <div className="kaisetsuki-schedule"> */}
+            <div className='kaisetsuki'>{subject["開設期"]}</div>
+            <div className='schedule'>{subject["曜日・時限・講義室"]}</div>
+            {/* </div> */}
+
+            <div className='campus-language'>
+                <div className="campus">{subject["開講キャンパス"]}</div>
+                <div className="language">{subject["使用言語"]}</div>
             </div>
-            <div className="lecture-details-info">
-                <div>{subject["授業の目標・概要等"]}</div>
-                <div>{subject["メッセージ"]}</div>
+
+            <div className='abstract'>
+                <div className="abstract-text">{subject["授業の目標・概要等"]}</div>
             </div>
         </div>
     );
--- src/table-view/SyllabusTable.tsx
+++ src/table-view/SyllabusTable.tsx
@@ -28,6 +28,16 @@ function SyllabusTable({ searchOptions, bookmarkedSubjects, handleBookmarkToggle
             <div className='table-wrapper'>該当授業数: {filteredSubjectCodeList(searchOptions).length}</div> {/* 行数を表示 */}
             <div className='table-wrapper'>表示数: {data.length} (/最大表示数: {maxNumberOfSubjectsToShow})</div> {/* 行数を表示 */}
 
+
+            <div className='lecture-details-header'>
+                <div className='star-button-header'></div>
+                <div className='lecture-code-name-header'>講義コード・授業科目名</div>
+                <div className='teacher-header'>担当教員</div>
+                <div className='kaisetsuki-header'>開設期</div>
+                <div className='schedule-header'>曜日・時限・講義室</div>
+                <div className='campus-language-header'>キャンパス・言語</div>
+                <div className='abstract-header'>授業の目標・概要等</div>
+            </div>
             {/* LectureUnit コンポーネントを使用して授業を表示 */}
             <div className="lectures-container">
                 {data.map((subject, index) => (
