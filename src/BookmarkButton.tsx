41bef6192584b3372e7f49aafbe4a55920e28402
swawa-yu <swawa.yu@gmail.com>
2024-01-02 09:39:28 +0900

    ブックマークボタンをコンポーネントに切り出した(react-tableに変更したとき)

---
+++ src/BookmarkButton.tsx
@@ -0,0 +1,16 @@
+import { useContext } from 'react';
+import { BookmarkContext, BookmarkContextType } from './contexts/BookmarkContext.tsx';
+
+
+const BookmarkButton = ({ lectureCode }: { lectureCode: string }) => {
+    const { bookmarkedSubjects, handleBookmarkToggle } = useContext<BookmarkContextType>(BookmarkContext);
+    const isBookmarked = bookmarkedSubjects.has(lectureCode);
+
+    return (
+        <div className="star-button" onClick={() => handleBookmarkToggle(lectureCode)}>
+            {isBookmarked ? "★" : "☆"}
+        </div>
+    );
+};
+
+export default BookmarkButton;
