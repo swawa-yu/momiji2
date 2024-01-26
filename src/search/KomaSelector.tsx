b986a737691e482206e9e1096401ad352f1b2d75
swawa-yu <swawa.yu@gmail.com>
2024-01-27 05:47:58 +0900

    TODOコメントを追加

--- src/search/KomaSelector.tsx
+++ src/search/KomaSelector.tsx
@@ -3,6 +3,7 @@ import './KomaSelector.css';
 
 
 // TODO: 土、6,7コマを追加すると「その他」の出番はなくなる
+// TODO: subject/types.tsにもyoubiの定義がある！
 export const youbis: Youbi[] = ["月", "火", "水", "木", "金", "土"];
 export const komas: Koma[] = [1, 2, 3, 4, 5, 6, 7];
 export const specialSchedules: SpecialSchedule[] = ["集中", "その他"];
