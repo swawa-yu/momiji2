2986e1ebc9f0396018c25fac67208ec6339a7244
swawa-yu <swawa.yu@gmail.com>
2025-04-13 09:14:03 +0900

    Revert "Merge pull request #33 from swawa-yu/feature/install-mui"
    This reverts commit 1f9bb588304fffb982e8a69fa9ff27173e845932, reversing
    changes made to 47961da555b1f6864fa128811e9398e4175fed6e.

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
-const theme = createTheme();
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 )
